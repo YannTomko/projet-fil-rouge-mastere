8e3d21e06d87b9177336d1420cd6af5a4e149aac
camilletouron <camille.touron.pro@gmail.com>
2025-02-27 11:44:04 +0100

    Statistic model in front and back

---
+++ client/src/models/Statistic.ts
@@ -0,0 +1,6 @@
+export interface Statistic {
+    nb_access_last_24h: number;
+    nb_access_last_week: number;
+    nb_access_total: number;
+    last_access_date_time: Date;
+}
---
+++ server/models/statistic.js
@@ -0,0 +1,8 @@
+export default class Statistic {
+    constructor(nb_access_last_24h, nb_access_last_week, nb_access_total, last_access_date_time) {
+        this.nb_access_last_24h = nb_access_last_24h;
+        this.nb_access_last_week = nb_access_last_week;
+        this.nb_access_total = nb_access_total;
+        this.last_access_date_time = last_access_date_time;
+    }
+}
