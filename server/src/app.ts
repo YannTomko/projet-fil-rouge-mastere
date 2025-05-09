b3ba7ce223c2b206cb58d77e9242fed351604196
unknown <yann.tomkowiak@gmail.com>
2025-05-09 15:25:46 +0200

    Ajout tests

---
+++ server/src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes";
+import filesRoutes from "./routes/filesRoutes";
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  const start = Date.now();
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+    console.log(`${req.method} ${req.originalUrl} => ${res.statusCode} (${duration} ms)`);
+  });
+  next();
+});
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/files", filesRoutes);
+
+export default app;
