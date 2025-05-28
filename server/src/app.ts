ee63dfb4d8043e5438d0ad97995262c75528f339
unknown <yann.tomkowiak@gmail.com>
2025-05-28 15:43:15 +0200

    Fix cicd tests

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes";
 import filesRoutes from "./routes/filesRoutes";
+import 'dotenv/config';
 
 const app = express();
 
